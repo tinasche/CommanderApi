89cbc11e4105b4428dce3079acac2942eca5e313
Tinashe Chitakunye <tinashec93@live.com>
2023-01-22 06:56:02 +0200

    feat: integrate with Azure CosmosDB

--- Program.cs
+++ Program.cs
@@ -3,13 +3,21 @@ using Microsoft.EntityFrameworkCore;
 using VersedApi.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+var MyAllowedOrigins = "_myAllowedOrigins";
 
 // Add services to the container.
-
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowedOrigins,
+    policy =>
+    {
+        policy.WithOrigins("https://localhost:5173", "https://commander-he41em2mi-tinasche.vercel.app/", "https://commander-app.vercel.app/");
+    });
+});
 builder.Services.AddControllers();
 builder.Services.AddRazorPages();
 builder.Services.AddDbContext<CommandsContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("CommandConn")).UseCamelCaseNamingConvention());
+    options.UseNpgsql(builder.Configuration.GetConnectionString("CosmosConn")).UseCamelCaseNamingConvention());
 builder.Services.AddScoped<ICommandsService, CommandsService>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -18,11 +26,12 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
 app.UseSwagger();
 app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
+
+app.UseCors(MyAllowedOrigins);
 
 app.UseAuthorization();
 
